a5266deb199ce986d27d80c41d53ba561f3ea9ad
Rajesh Ranganathan <rajesh.ranganathan@hpe.com>
2020-12-04 23:13:57 +0530

    Started with changes for Command Parser

--- ec2/kundu.py
+++ ec2/kundu.py
@@ -1,8 +1,20 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='cguru')
+ec2 = session.resource('ec2')
+
+
+@click.command()
+def list_instances():
+    "List ec2 instances"
+    for i in ec2.instances.all():
+        print(','.join((
+              i.id,
+              i.instance_type,
+              i.placement['AvailabilityZone'],
+              i.state['Name'],
+              i.public_dns_name)))
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='cguru')
-    ec2 = session.resource('ec2')
-
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
