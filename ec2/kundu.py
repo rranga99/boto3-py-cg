abaa6720bb1e553d1e61104435d527b55d8f0749
Rajesh Ranganathan <rajesh.ranganathan@hpe.com>
2020-12-06 22:10:48 +0530

    Changes for handling incorrect states during stop or start of instances

--- ec2/kundu.py
+++ ec2/kundu.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='cguru')
@@ -99,7 +100,7 @@ def create_snapshots(project):
 
         i.start()
         i.wait_until_running()
-    
+
     print("All done!")
 
     return
@@ -137,7 +138,11 @@ def start_instances(project):
 
     for i in instances:
         print('Starting {}'.format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Could not start {0}".format(i.id) + str(e))
+            continue
 
     return
 
@@ -152,8 +157,11 @@ def start_instances(project):
 
     for i in instances:
         print('Stopping {}'.format(i.id))
-        i.stop()
-
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Could not stop {0}".format(i.id) + str(e))
+            continue
     return
 
 
