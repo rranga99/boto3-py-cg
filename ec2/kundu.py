3974bf084f2e06d71ab5713e2090a424b0a9268f
Rajesh Ranganathan <rajesh.ranganathan@hpe.com>
2020-12-06 22:38:52 +0530

    Changes to select snapshots based on flags

--- ec2/kundu.py
+++ ec2/kundu.py
@@ -57,7 +57,9 @@ def snapshots():
 @snapshots.command('list')
 @click.option('--project', default=None,
               help="Only snapshots for the project (tag Project: <name>)")
-def list_snapshots(project):
+@click.option('--all', 'list_all', default=False, is_flag=True,
+              help="List all snapshots for each volume, including oldest")
+def list_snapshots(project, list_all):
     "List EC2 snapshots"
 
     instances = filter_instances(project)
@@ -73,6 +75,9 @@ def list_snapshots(project):
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
+
+                if s.state == "completed" and not list_all:
+                    break
 
     return
 
